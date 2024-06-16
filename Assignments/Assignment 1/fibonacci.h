24f83d1159b69c9f5ec8145257798a0b21906f1a
Benjamin, Taylor, Schaefer-Jackson-Stepenekov <tbsjackson333@protonmail.ch>
2024-06-16 15:34:23 +0300

    Update 20240616

--- Assignments/Assignment 1/fibonacci.h
+++
@@ -1,7 +0,0 @@
-#ifndef _FIBONACCI_H
-#define _FIBONACCI_H
-
-int fib(int n);
-void print_fib(int n);
-
-#endif
--- Assignments/Assignment 1/reverse.h
+++
@@ -1,6 +0,0 @@
-#ifndef _REVERSE_H
-#define _REVERSE_H
-
-void reverse(char *str, int len);
-
-#endif
